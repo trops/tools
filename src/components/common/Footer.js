83e5bedda6ae0fb06dc1fad5b6464569d3a5d378
John Giatropoulos <john.giatropoulos@gmail.com>
2022-04-18 12:53:36 -0400

    main, added version to footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -7,8 +7,7 @@ class Footer extends React.Component {
     return (
       <div className="flex flex-row p-4 bg-gray-800 w-full justify-center mt-auto text-xs text-center items-center">
         {/* {chosen !== null && (<div className="bg-gray-600 text-white p-2 text-xs rounded-sm cursor-pointer" onClick={onCancel}>Home</div>)} */}
-        <span className="text-gray-600">@trops</span>
-
+        <span className="text-gray-600">@trops - v{process.env.REACT_APP_VERSION}</span>
       </div>
     )
   }
