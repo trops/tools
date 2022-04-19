90ad50ba276621dd6f330969a97ab5c0ad17e49a
John Giatropoulos <john.giatropoulos@gmail.com>
2022-04-18 23:03:08 -0400

    xml-split, Updated xml parser to utilize chunks

--- public/lib/dialogController.js
+++ public/lib/dialogController.js
@@ -1,11 +1,9 @@
 const { dialog } = require('electron');
 const fs = require('fs');
-const xtreamer = require('xtreamer');
-const { transformer } = require('./xmlTransform');
 const sax = require('sax');
+const XMLSplitter = require('xml-splitter');
 
 let count = 0;
-
 /**
  * showDialogChooseXMLFile
  * @param {*} win 
@@ -18,55 +16,63 @@ const showDialogChooseXMLFile = (win, message) => {
       const filepath = result.filePaths[0];
       const extension = filepath.substr(filepath.lastIndexOf('.') + 1);
       if (extension === 'xml') {
+        count = 0;
         const toFilepath = filepath.substring(0, filepath.length - 4) + '.json';
         if (filepath) {
-          count = 0;
           win.webContents.send('choseXMLFile', { filepath, toFilepath });
         } else {
-          alert('Could not open file');
+          win.webContents.send('parseXMLFileChosenError', { 
+            message: "Could not open file" 
+          });
         }
       } else {
-        win.webContents.send('parseXMLFileChosenError', { message: "Please choose an XML file." } );
+        win.webContents.send('parseXMLFileChosenError', { 
+          message: "Please choose an XML file." 
+        });
       }
     }).catch(e => console.log(e));
 }
 
-
+/**
+ * parseXMLFileChosen
+ * Parse the file initially and find the nodes for the user to choose
+ */
 const parseXMLFileChosen = (win, message) => {
   const { filepath, toFilepath } = message;
   if (filepath && toFilepath) {
     parseXMLFileSaxTags(filepath, win);
   } else {
-    win.webContents.send('parseXMLFileChosenError', { message: "There was an error parsing the nodes." } );
+    win.webContents.send('parseXMLFileChosenError', { 
+      message: "There was an error parsing the nodes." 
+    });
   }
 }
 
 const parseXMLFileSaxTags = (xmlFilePath, win) => {
   var fileStream = fs.createReadStream(xmlFilePath);
   var saxParser = sax.createStream(true);
-
   const tagsFound = [];
-  
   saxParser.on('opentag', data => {
     // let's keep adding tags until we have a duplicate.
     // this should have iterated through the root and one child node...
     if (tagsFound.indexOf(data.name) < 0) {
       tagsFound.push(data.name);
-      win.webContents.send('parseXMLFileChosenTagFound', { message: "Found node", node: data.name } );
+      win.webContents.send('parseXMLFileChosenTagFound', { 
+        message: "Found node", node: data.name });
     }
   });
 
   saxParser.on('end', data => {
     // if we have reached the end, destroy the stream
-    
-    win.webContents.send('parseXMLFileChosenTagsFound', { message: "We have reached the end of the road", tags: tagsFound } );
+    win.webContents.send('parseXMLFileChosenTagsFound', { 
+      message: "We have reached the end of the road", tags: tagsFound 
+    });
     fileStream.close();
     fileStream.destroy();
   });
 
   saxParser.on('error', data => {
     // error, kill it
-    
     win.webContents.send('parseXMLFileChosenError', { message: "Error Found" } );
     fileStream.close();
     fileStream.destroy();
@@ -77,23 +83,27 @@ const parseXMLFileSaxTags = (xmlFilePath, win) => {
 }
 
 /**
- * 
+ * parseXMLFileChosenWithNode
+ * Parse the xml file based on a chosen node
  * @param {*} xmlFilePath 
  * @param {*} win 
  */
-
 const parseXMLFileChosenWithNode = (win, message) => {
   const { filepath, toFilepath, nodeToExtract } = message;
   if (filepath && toFilepath && nodeToExtract) {
     parseXMLFile(filepath, toFilepath, nodeToExtract, win);
   } else {
-    win.webContents.send('parseXMLFileChosenError', { message: "Hmm, it appears your paths are incorrect" } );
+    win.webContents.send('parseXMLFileChosenError', { 
+      message: "Hmm, it appears your paths are incorrect" 
+    });
   }
 };
 
 /**
  * parseXMLFile
  * Parse the filepath to the XML
+ * - first we have to split the file into workable chunks
+ * - experiment...
  * @param {string} xmlFilePath the filepath to the XML
  */
 const parseXMLFile = (xmlFilePath, jsonFilepath, nodeToExtract, win) => {
@@ -101,36 +111,33 @@ const parseXMLFile = (xmlFilePath, jsonFilepath, nodeToExtract, win) => {
   // input readable stream with event handlers
   const readStream = fs.createReadStream(xmlFilePath);
   const writeStream = fs.createWriteStream(jsonFilepath);
-  
+
+  // begin the JSON file  
   writeStream.write('[\n');
-  // input | transform
-  readStream.pipe(xmlStreamer(nodeToExtract, { transformer: transformer, max_xml_size: 100000000 }, writeStream, win))
+
+  // read, split, and write the file
+  readStream.pipe(xmlSplitterStream(nodeToExtract, writeStream, win).stream);
 }
 
-/**
- * xmlStreamer
- * The streamer to pass into the pipe in order to extract the correct XML
- * @param {string} node the XML node to extract from the file
- * @returns 
- */
-const xmlStreamer = (node, jsonTransformer, jsonWriter, win) => {
-  return xtreamer(node, jsonTransformer)
-    .on("data", (data) => {
+const xmlSplitterStream = (nodeToExtract, writeStream, win) => {
+  // event listeners for the xmlSplitter
+  return new XMLSplitter(`//${nodeToExtract}`)
+    .on('data', (node, tag, path) => {
       win.webContents.send('xmlDataParsed', 'got xml data');
-      jsonWriter.write(parseChunk(data));
-      count++;
-    })
-    .on("end", () => {
-      jsonWriter.write('\n]');
+      writeStream.write(parseChunk(node));
+    }).on("end", () => {
+      writeStream.write('\n]');
+      writeStream.end();
       win.webContents.send('xmlToJsonComplete','');
     })
     .on("error", (error) => {
       win.webContents.send('xmlToJsonError', error.message);
+      writeStream.end();
     });
 }
 
 const parseChunk = (chunk) => {
-  return (count > 0 ? ',\n' : '') + chunk.toString();
+  return (count > 0 ? ',\n' : '') + JSON.stringify(chunk).replaceAll('$t', "_value");
 }
 
 module.exports = { 
