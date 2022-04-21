b83d1dab77fc4006652542bc4b9588c6a5eb6810
John Giatropoulos <john.giatropoulos@gmail.com>
2022-04-21 16:05:00 -0400

    refactor-lib, small changes cleanup

--- public/electron.js
+++ public/electron.js
@@ -1,7 +1,7 @@
 const path = require('path');
-const { app, BrowserWindow, ipcMain, Menu } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const isDev = require('electron-is-dev');
-const { parseXmlMainEvents, applicationMenuEvents } = require('./lib/events');
+const { parseXmlMainEvents } = require('./lib/events');
 
 // My library of functions
 const { 
@@ -10,7 +10,7 @@ const {
   parseXMLFileChosenWithNode 
 } = require('./lib/controller');
 
-const { applicationMenu } = require('./lib/controller');
+// const { applicationMenu } = require('./lib/controller');
 
 let win;
 
